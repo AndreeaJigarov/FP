58c605b2eac69050a16a6b90ad549fd92e582c81
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2024-10-03 11:51:29 +0300

    Update seminar_1.py

--- src/seminar/group914/seminar_1.py
+++ src/seminar/group914/seminar_1.py
@@ -1,17 +1,5 @@
 """
 FP seminar 1 problems & solutions
-
-How to set up things!
-    1. An installation of Python 3
-    2. An IDE --> PyCharm
-    3. A GitHub account -- needed to accept assignments
-"""
-
-print("Hello World!")
-"""
-Process finished with exit code 0
-    - you need to see this to make sure the program has stopped :)
-    - exit code 0 means no errors 
 """
 
 """
@@ -20,20 +8,69 @@ Problem statements to choose from:
     https://www.freecodecamp.org/news/python-coding-challenges-for-beginners/
 """
 print ("Hello")
+
+
 """
 1. Given 2 ints, a and b, return True if one of them is 10 or if their sum is 10
 Question – What happens if we enter a non-integer number, or alphanumeric characters?
 """
-a = int(input ("First value")) # input returns a string (python string type)
-b = int(input (" second value"))
-print(a+b) 
+# a = int(input ("First value")) # input returns a string (python string type)
+# b = int(input (" second value")) # int is a function that comnverts a string into int
+# print(a+b) 
 
+"""
+the function below uses Python type hints
+ a: int (means that the type of parameter a should be int)
+ -> bool ( means that the functions should return a value of type bool)
+"""
+#def is_sum_10(a: int ,b: int) -> bool :
+ #   if a == 10 or b == 10 or a+b ==10:
+  #      return "true"
+ #   else:
+ #       return "false"
+
+#is sum 10 (a, b)
+# a= "agcrddr"
+# a= 2346   
+"""
+static= at compile  time
+dynamic = at run time ( Python is a dinamically typed language)
+
+strongly typed language = at every moment, every variable has a well known type
+in python the type is assigned at the same time as the value, so it can change as in that example
+"""
 
 """
 2. Write a Python program which iterates the integers from 1 to 50. 
 For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
 For numbers which are multiples of both three and five print "FizzBuzz".
 """
+
+# Version 1 without functions
+for i in range ( 1, 51):
+    if i % 3 and i%5:
+          print ( "Fizz&Buzz")
+    elif i % 3 == 0:
+          print ("Fizz")
+    elif i% 5 == 0:
+         print ("Buzz")
+    else:
+         print( i)
+
+
+#  Version 2 with functions
+def fizz_buzz ( left: int, right: int) -> list:
+     result= [] # empty list in python
+for i in range ( left, right +1):
+    if i % 3 and i%5:
+          result.append ( "Fizz&Buzz")
+    elif i % 3 == 0:
+          result.append("Fizz")
+    elif i% 5 == 0:
+         result.append("Buzz")
+    else:
+         result.append( i)
+return result
 
 """
 3. Calculate the first n terms of the Fibonacci sequence
